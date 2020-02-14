9262e415fc0897e0b51f24170591171b94907c88
Ilya VVV <Ilya@mail.ru>
2020-02-14 18:12:55 +0300

    fix

--- tests/diffTest.php
+++ tests/diffTest.php
@@ -20,9 +20,11 @@ class DiffTest extends TestCase
     {
         $path = 'tests/fixtures/';
         return [
-            'testPrettyJson' => [$path . 'expectedDiffTree', $path . 'beforeTree.json', $path . 'afterTree.json', 'pretty'],
+            'testPrettyJson' => [$path . 'expectedDiffTree', $path . 'beforeTree.json', $path . 'afterTree.json',
+            'pretty'],
             'testJaml' => [$path . 'expectedDiff', $path . 'before.yaml', $path . 'after.yaml', 'pretty'],
-            'testPlainTree' => [$path . 'expectedTreePlain', $path . 'beforeTree.json', $path . 'afterTree.json', 'plain'],
+            'testPlainTree' => [$path . 'expectedTreePlain', $path . 'beforeTree.json', $path . 'afterTree.json',
+            'plain'],
             'testJson' => [$path . 'expectedJson', $path . 'before.json', $path . 'after.json', 'json']
             ];
     }
